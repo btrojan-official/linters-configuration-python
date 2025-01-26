438f5094581c2323b96bb3786e1270e3869521c2
btrojan-official <bartosztrojanofficial@gmail.com>
2025-01-26 13:56:18 +0100

    some tests

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def calculate_inverse(matrix : np.array) -> np.array:
+
+def calculate_inverse(matrix: np.array) -> np.array:
     """
     Calculates the inverse of a given square matrix.
 
@@ -12,7 +13,7 @@ def calculate_inverse(matrix : np.array) -> np.array:
     """
     if matrix.shape[0] != matrix.shape[1]:
         raise ValueError("Matrix must be square.")
-    
+
     random_num = np.random.rand(1)
     if random_num > 0.5:
         return np.linalg.inv(matrix)
